f770e2a9f47b5c29de016846c84e82ffd9a1d6d3
Arnaud GUILLARD <arguilla@e2r6p17.42.fr>
2021-08-23 23:27:53 +0200

    delete the libft git submodule

--- srcs/main.c
+++ srcs/main.c
@@ -1,9 +1,23 @@
 #include "push_swap.h"
 
-int	main(int ac, char **av)
+typedef struct	s_stack
 {
-	(void)ac;
-	(void)av;
-	printf("len : %ld\n", ft_strlen("coucou"));
-	printf("%d\n", ft_atoi("+12"));
+	int				nb;
+	struct s_stack	*next;
+}				t_stack;
+
+typedef struct	s_ps
+{
+	t_stack	*a;
+	t_stack	*b;
+}				t_ps;
+
+int	main()
+{
+	t_ps	*ps;
+
+	if (!(ps = malloc(sizeof(t_ps))))
+		return (1);
+	ps->a = ft_lstnew(5);
+	printf("%d", ps->a->nb);
 }
